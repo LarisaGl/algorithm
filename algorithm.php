c4cabfd0c6378f348b2e1e5eeb637fed6d7151af
LarisaGl <89888985265@mail.ru>
2018-05-07 22:42:16 +0300

    07/05

--- algorithm.php
+++ algorithm.php
@@ -6,23 +6,28 @@ echo "Число ".$x."<br>";
 $a=1;
 $b=1;
 
-if ($a>$x) {
-	echo "Задуманное число НЕ входит в числовой ряд";
-}
+while (true) {
+	if ($a>$x) {
+		echo "Задуманное число НЕ входит в числовой ряд"."<br>";
+		break;
+	}
     else {
-      if ($a==$x){
-      	echo "Задуманное число входит в числовой ряд";
-      }
-      else {
-      	$c=$a;
-      	echo $c."<br>";
-      	echo $a."<br>";
-      	$a=$a+$b;
-      	echo $a."<br>";
-      	echo $b."<br>";
-      	$b=$c;
-      	echo $b."<br>";
-      	echo $c."<br>";
-      }
+        if ($a==$x) {
+            echo "Задуманное число входит в числовой ряд"."<br>";
+            break;
+        }
+            else {
+               	$c=$a;
+      			echo $c."<br>";
+      			echo $a."<br>";
+      			$a=$a+$b;
+      			echo $a."<br>";
+      			echo $b."<br>";
+      			$b=$c;
+      			echo $b."<br>";
+      			echo $c."<br>";
+     		 }
     }
+}
+
 ?>
